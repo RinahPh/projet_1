60c83ca2041509dde28709420d0ffc28cf17bb44
Rabedaoro David N H <rdavidnh@gmail.com>
2019-10-01 22:56:05 +0300

    modification exo 2.9. Un programme qui dit bonjour de manière complète

--- exo_09.rb
+++ exo_09.rb
@@ -1,5 +1,9 @@
+# 2.9. Un programme qui dit bonjour de manière complète
+
 puts "Entrer votre prénom"
+print "> "
 prenom = gets.chomp
 puts "Entrer votre Nom"
+print "> "
 nom = gets.chomp
-puts "Bonjour , #{prenom + nom}"
+puts "Bonjour, #{prenom + ' ' + nom}"
