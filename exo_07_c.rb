e37bd28901a67b3354db34b765bc86f22e14c608
Rabedaoro David N H <rdavidnh@gmail.com>
2019-10-01 22:52:19 +0300

    correction exo 2.7. Demander à l'utilisateur

--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,3 +1,6 @@
 user_name = gets.chomp
 puts user_name
-#les differences entre les 3  
+#les differences entre les 3 programmes sont
+# "a" demande à l'utilisateur d'entrer une valeur mais n'indique pas la valeur entrée
+# "b" demande à l'utilisateur d'entrer une valeur et utilise "print >" pour indiquer la valeur entrée
+# "c" ne demande pas à l'utilisateur d'entrer une valeur et n'indique pas la valeur entrée
